604b9630095ddde04debb5ac30b551e53f7a0416
harsh upasani <harshyupasani@gmail.com>
2018-09-12 19:57:00 +0530

    slider click interference bug fix

--- app/src/main/java/com/harsh/influx/utils/AnimUtil.java
+++ app/src/main/java/com/harsh/influx/utils/AnimUtil.java
@@ -1,12 +1,15 @@
 package com.harsh.influx.utils;
 
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 
 public class AnimUtil {
 
     // slide the view from below itself to the current position
     public static void slideUp(View view){
+        ((ViewGroup)view.getParent()).setVisibility(View.VISIBLE);
         view.setVisibility(View.VISIBLE);
         TranslateAnimation animate = new TranslateAnimation(
                 0,                 // fromXDelta
@@ -20,7 +23,7 @@ public class AnimUtil {
     }
 
     // slide the view from its current position to below itself
-    public static void slideDown(View view){
+    public static void slideDown(final View view){
         TranslateAnimation animate = new TranslateAnimation(
                 0,                 // fromXDelta
                 0,                 // toXDelta
@@ -28,9 +31,27 @@ public class AnimUtil {
                 view.getHeight()); // toYDelta
         animate.setDuration(500);
         animate.setFillAfter(true);
+        animate.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                view.setVisibility(View.GONE);
+                ((ViewGroup)view.getParent()).setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
         view.startAnimation(animate);
         view.setTag(false);
         view.setVisibility(View.GONE);
+        ((ViewGroup)view.getParent()).setVisibility(View.GONE);
     }
 
     public static void toggleSlide(View view) {
